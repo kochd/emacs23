0c0bf26975e4ebe003c5dabc8aa6312e1e737ea4
Daniel Koch <koch@triple6.org>
2014-01-17 20:50:26 +0100

    updated repos

---
+++ .emacs.d/elpa/qml-mode-20130417.744/qml-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; qml-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (qml-mode) "qml-mode" "qml-mode.el" (21202 38793
+;;;;;;  483553 401000))
+;;; Generated autoloads from qml-mode.el
+
+(autoload 'qml-mode "qml-mode" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("qml-mode-pkg.el") (21202 38793 565441
+;;;;;;  866000))
+
+;;;***
+
+(provide 'qml-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; qml-mode-autoloads.el ends here

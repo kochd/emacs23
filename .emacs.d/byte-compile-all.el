2e92c7680303a0432373dcf0d85448e8151130f6
Daniel Koch <koch@triple6.org>
2014-03-12 07:28:18 +0100

    cleanup

--- .emacs.d/byte-compile-all.el
+++
@@ -1,9 +0,0 @@
-;; batch byte compile of all elisp (.el) files in a dir and subdirs
-;; Eval this buffer to use it
-(require 'find-lisp)
-
-(mapc
- (lambda (x) (byte-compile-file x))
- (find-lisp-find-files
-  (expand-file-name (concat (file-name-directory (or load-file-name buffer-file-name)) default-directory))
-  "\\.el$"))
